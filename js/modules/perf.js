5fab2f141e5ebbe3654c78d427305c96476fec1d
PRIYASH DAS <154573843+Priyash-Das@users.noreply.github.com>
2026-06-13 03:47:33 +0530

    Delete js directory

--- js/modules/perf.js
+++
@@ -1,6 +0,0 @@
-const cores = navigator.hardwareConcurrency || 8,
-  memory = navigator.deviceMemory || 8;
-export const isLowEnd = cores < 2 || memory <= 4;
-export function applyPerfMode() {
-  if (isLowEnd) document.documentElement.classList.add("perf-lite");
-}
