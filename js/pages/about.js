85e6a6d38f791bfc04f9ede5e01c64d304ffb194
PRIYASH DAS <154573843+Priyash-Das@users.noreply.github.com>
2026-06-13 09:01:14 +0530

    Update

--- js/pages/about.js
+++ js/pages/about.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
       " ",
       " ",
       "ALMOST THERE",
-      ". . . . . . . . . . . . . . . . . . . .",
+      ". . . . . . . . . . . . . . . . .",
       " ",
       " ",
       "PAGE IS LOADED SUCCESSFULLY",
